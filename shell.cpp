3c68100c77a41d6c0fe9ac7d0195fa07d4b63468
slifer4195 <54692613+slifer4195@users.noreply.github.com>
2021-10-12 02:34:13 -0400

    Add files via upload

--- shell.cpp
+++ shell.cpp
@@ -6,7 +6,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <vector>
-
+#include <fcntl.h>
 using namespace std;
 
 string trim (string input){
@@ -50,21 +50,96 @@ vector<string> split(string line, string seperators = " "){
             count += 1;
         }
     }
+
+    for (int j = 0; j < count + 1; j++){
+        index = newline.find(seperators);
+        splVec.push_back(newline.substr(0, index));
+        newline =newline.substr(index+1);
+    }
+    return splVec;
+}
+
+bool input(string inputline ){
+     vector<string> parts = split(inputline);
+    for (int i = 0; i < parts.size(); i++){
+        if (parts.at(i) == "<") return true;
+    }
+    return false;
+}
+
+bool output(string inputline){
+     vector<string> parts = split(inputline);
+    for (int i = 0; i < parts.size(); i++){
+        if (parts.at(i) == ">") return true;
+    }
+    return false;
+}
+
+void execute(string inputline) {
+    vector<string> parts = split(inputline);
+    char** args = vec_to_char_array(parts);
+    int fd;
+    execvp(args [0], args);
+}
+
+string directOutput(string inputline){
+    string seperator = ">";
+    vector<string> parts = split(inputline, seperator);
+    string command = parts.at(0);
+    char* output = (char*)parts.at(1).c_str();
+    int fd = open(output,O_CREAT|O_WRONLY|O_TRUNC,
+    S_IRUSR| S_IWUSR | S_IRGRP |S_IROTH);
+    dup2(fd, 1);
+    close(fd);
+    return command;
+}
+
+string directInput(string inputline){
+    string seperator = "<";
+    vector<string> parts = split(inputline, seperator);
+    string command = parts.at(0);
+    char* output = (char*)parts.at(1).c_str();
+    int fd = open(output,O_CREAT|O_RDONLY,
+    S_IRUSR|S_IRGRP |S_IROTH);
+    dup2(fd, 0);
+    close(fd);
+    return command;
+}
+
+string outputInput(string inputline){
+    string seperatorOutput = ">";
+    vector<string> parts = split(inputline, seperatorOutput);//parts(0) grep rcu < a parts(1)  b
+
+    string seperatorInput = "<";
+    vector<string> line = split(parts.at(0), seperatorInput);//parts(0) grep rcu  parts(1) a
+
+    char* input = (char*)line.at(1).c_str();  // a
+    char* output = (char*)parts.at(1).c_str();  // a
+
+    int fd1 = open(input,O_CREAT|O_RDONLY, 
+    S_IRUSR|S_IRGRP |S_IROTH);
+
+    int fd2 = open(output, O_CREAT|O_WRONLY|O_TRUNC,
+    S_IRUSR| S_IWUSR | S_IRGRP |S_IROTH);
+    dup2(fd1, 0);
+    dup2(fd2, 1);
+    execute(line.at(0));
+    
+    string command = line.at(0);
+    return command;
 }
 
 int main(){
     vector<int> bgs;
     while (true){
         for (int i = 0 ; i < bgs.size();i++){
-            if (waitpid (bgs [i], 0,WNOHANG) < 0){
-                ;
-            }else{
+            if (waitpid (bgs [i], 0,WNOHANG) == bgs[i]){
                 bgs.erase (bgs.begin() + i);
                 i--;
             }
         }
         cout << "My shells$: ";
-        string inputline;
+        string inputline; 
         getline(cin, inputline);
         if (inputline == string("exit")){
             cout << "Bye!! End of shell" << endl;
@@ -75,23 +150,41 @@ int main(){
         inputline = trim(inputline);
 
         if (inputline[inputline.size()-1] == '&'){
-            cout << "BG process found" << endl;
+            // cout << "BG process found" << endl;
             bg = true;
             inputline = inputline.substr (0, inputline.size() - 1);
         }
 
         if (pid == 0){
+            // execute() 
+            string command;
             vector<string> parts = split(inputline);
-            char** args = vec_to_char_array(parts);
-            execvp(args [0], args);
+            cout << output(inputline) << endl;
+            if (input(inputline) && output(inputline)) {
+                //redirect input and output
+                cout << "input and output" << endl;
+                command = outputInput(inputline);
+                execute(command);
+            }
+            else if (input(inputline)){
+                //redirect input
+                command = directInput(inputline);
+                execute(command);
+            }
+            else if (output(inputline)){
+                //redirect input
+                command = directOutput(inputline);
+                execute(command);
+            }
+
         }else{
             if (!bg)
                 waitpid(pid, 0,0);
-            else{
+             else{
                 bgs.push_back(pid);
             }
         }
-
     }
+
     return 0;
 }
